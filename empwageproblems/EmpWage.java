e4d4fdd2d30dae097f5445208bd6b4ceac3e1b34
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-24 23:08:58 +0530

    Added: Calculate Employee Wage using Switch Case

--- empwageproblems/EmpWage.java
+++ empwageproblems/EmpWage.java
@@ -2,19 +2,21 @@ package empwageproblems;
 
 public class EmpWage {
 	public static void main(String[] args) {
-		int is_FULL = 1;
-		int is_PART = 2;
 		int empRatePerHr = 20;
 		int empWorkingHr;
 		double empCheck = Math.floor(Math.random() * 10) % 3;
-	    if(empCheck == is_FULL) 
-			empWorkingHr = 8; 
-		else if(empCheck == is_PART)
-		    empWorkingHr = 4;
-		 else
-		    empWorkingHr = 0;
-	    int salary = empRatePerHr * empWorkingHr;   
-	    System.out.println("Salary is :" + salary);	
-	}
-
+		int i = (int)empCheck;
+		switch(i) {
+		case 1:
+			empWorkingHr = 8;
+			break;
+		case 2:
+			empWorkingHr = 4;
+			break;
+		default:
+			empWorkingHr = 0;
+		}
+	int salary = empWorkingHr  * empRatePerHr;
+	System.out.println("Salary is :" + salary);
+	}		
 }
