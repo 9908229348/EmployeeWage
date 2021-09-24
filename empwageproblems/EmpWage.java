b09d4d915499d9d37e5f681b64d20b574563268a
9908229348 <kalavaisravanthi98@gmail.com>
2021-09-24 22:41:34 +0530

    Added: Functionality to calculate Daily Employee Wage

--- empwageproblems/EmpWage.java
+++ empwageproblems/EmpWage.java
@@ -3,12 +3,18 @@ package empwageproblems;
 public class EmpWage {
 	public static void main(String[] args) {
 		int is_FULL = 1;
+		int empRatePerHr = 20;
+		int empWorkingHr = 8;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == is_FULL)
-		   System.out.println("Employee is Present");
-		   else
-			   System.out.println("Employee is absent");
-		   
+		if(empCheck == is_FULL) {
+			System.out.println("Employee is present");
+		    int salary = empRatePerHr * empWorkingHr;
+		    System.out.println("Salary is :" +salary);
 		}
-
+		    else
+		    {
+		    	System.out.println("Employee is absent");
+		        System.out.println("Salary is 0");
+		    }    
+		}
 	}
